1bb33c86038ab06bdc30e504224e8ad4bdb101ba
Vinicius Assis <vinicius.coassis@gmail.com>
2024-06-03 16:38:45 -0300

    feat: add logger to router

--- backend/app/routes/v1/just_ai_router.py
+++ backend/app/routes/v1/just_ai_router.py
@@ -5,19 +5,18 @@ from app.service.just_ai_service import JustAiService
 from app.models.user_request_model import UserRequest
 
 router = APIRouter()
-# logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 # Rota para processar a solicitação do usuário
 @router.post("/legal-advice")
 def get_legal_advice(request: UserRequest):
 
-    # logger.info(f"[{__name__}] - Received request: {request}")
+    logger.info(f"[{__name__}] - Received request: {request}")
     
     justAiService = JustAiService()
     response = justAiService.execute(request)
     
-    # logger.info(
-    # f"[{__name__}] - Generate response:response={response}")
+    logger.info(f"[{__name__}] - Generate response:response={response}")
 
 
     return {"response": response}
